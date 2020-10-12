bbe03e69294e0b207c0c303cbcfcdf0677269f46
Zsigmons <44140531+Zsigmons@users.noreply.github.com>
2020-10-12 16:46:44 +0200

    Add files via upload

---
+++ master_receive.ino
@@ -0,0 +1,18 @@
+#include <Wire.h>
+char* x = "szijasimon";
+char z;
+void setup() {
+  // Start the I2C Bus as Master
+  Wire.begin(); 
+  Serial.begin(9600);
+}
+void loop() {
+  
+   Wire.requestFrom(9, 5);    // request 6 bytes from slave device #8
+
+  while (Wire.available()) { // slave may send less than requested
+    z = Wire.read(); // receive a byte as character
+    Serial.print(z);         // print the character
+    
+  }delay(500);
+  Serial.println();}
---
+++ master_send.ino
@@ -0,0 +1,16 @@
+#include <Wire.h>
+char* x = "szijasimon";
+void setup() {
+  // Start the I2C Bus as Master
+  Wire.begin(); 
+  Serial.begin(9600);
+}
+void loop() {
+  Wire.beginTransmission(9); // transmit to device #9
+  Wire.write(x);              // sends x 
+  Wire.endTransmission();    // stop transmitting
+  Wire.beginTransmission(8);
+  Wire.write(x);
+  Wire.endTransmission();
+  delay(500);
+}
